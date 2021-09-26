03e400de9e1e7e122642c6863d48c05c7c4d94fa
jung <asas6306@naver.com>
2021-09-26 17:40:46 +0900

    NeedLoginInterceptor 목적지 변경

--- src/main/java/com/example/demo/interceptor/NeedLoginInterceptor.java
+++ src/main/java/com/example/demo/interceptor/NeedLoginInterceptor.java
@@ -40,8 +40,7 @@ public class NeedLoginInterceptor implements HandlerInterceptor {
 				response.setContentType("text/html; charset=UTF-8");
 				response.getWriter().append("<script>");
 				response.getWriter().append("alert('" + resultMsg + "');");
-				response.getWriter().append("location.replace('/usr/member/login?redirectUri="
-						+ Util.getUriEncoded(rq.getCurrentUri()) + "');");
+				response.getWriter().append("history.back();");
 				response.getWriter().append("</script>");
 				// 리턴 false;를 이후에 실행될 인터셉터와 액션이 실행되지 않음
 			} else {
